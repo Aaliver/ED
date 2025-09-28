0c85217c53b780bccf22303a292201f8ec753321
Valeria Becerril <aaliver@ciencias.unam.mx>
2025-09-28 15:57:11 -0600

    feat: recursion with lists

---
+++ Practica_5/Aux.hs
@@ -0,0 +1,60 @@
+module Aux where
+
+{- Función: esPar
+   Descripción: Identifica si un número es par y regresa V o F
+   Uso: esPar 3 = False
+-}
+esPar :: Int -> Bool
+esPar x = x `mod` 2 == 0
+
+
+{- Función: suma
+   Descripción: Regresa la suma de dos números
+   Uso: suma 16 7 = 23
+        suma 8 (-2) = 6
+        suma (-11) 4 = -7
+        suma (-7) (-8) = -15
+-}
+suma :: Int -> Int -> Int
+suma 0 m = m
+suma n m =
+  if n > 0
+  then suma (n - 1) (m + 1)
+  else suma (n + 1) (m - 1)
+
+
+{- Función: multi
+   Descripción: Regresa la multiplicación de dos números
+   Uso: multi 9 2 = 18
+-}
+multi :: Int -> Int -> Int
+multi m 0 = 0
+multi n m = suma n (multi n (m - 1))
+
+
+{- Función: suma
+   Descripción: Regresa la resta de dos números
+   Uso: resta 20 5 = 15
+        resta (-6) 2 = -8
+        resta 4 (-11) = 15
+        resta (-6) (-8) = 2
+-}
+resta :: Int -> Int -> Int
+resta n 0 = n
+resta n m =
+  if m > 0
+  then resta (n - 1) (m - 1)
+  else resta (n + 1) (m + 1)
+
+
+{- Función: dividirPar
+   Descripción: Regresa la división de dos números pares
+   Uso: dividirPar 40 2 = 20
+-}
+dividirPar :: Int -> Int -> Int
+dividirPar n m 
+  | m == 0    = error "No se puede divir entre cero"
+  | n == m    = 1
+  | otherwise = suma 1 (dividirPar (resta n m) m)
+
+  
---
+++ Practica_5/Practica5.hs
@@ -0,0 +1,74 @@
+module Practica5 where
+
+import Data.Char (toUpper)
+import Aux (esPar, suma, multi, dividirPar)
+
+{- Función: hollerBack
+   Desripción: Devuelve la cadena dada en mayusculas
+   Uso: hollerBack haskell = HASKELL
+-}
+hollerBack :: String -> String
+hollerBack "" = ""
+hollerBack (x:xs) = (toUpper x):(hollerBack xs)
+
+
+{- Función: palindromo
+   Desripción: Regresa un valor booleano dependiendo de si la
+   lista dada es palindroma o no
+   Uso: palindromo ['a','n','i','l','i','n','a'] = True
+-}
+palindromo :: Eq a => [a] -> Bool
+palindromo [] = True
+palindromo [x] = True
+palindromo (x:xs) =
+  if x == (last xs)
+  then palindromo (init xs)
+  else False
+
+
+{- Función: replica
+   Desripción: Regresa una lista con m elementos de n 
+   Uso: replica 2 5 = [2,2,2,2,2]
+-}
+replica :: Int -> Int -> [Int]
+replica _ 0 = []
+replica n m = n:(replica n (m - 1))
+
+
+{- Función: recuperaElemento
+   Desripción: Devuelve un elemento de la lista según su índice
+   Uso: recuperaElemento [2,4,6] 1 = 4
+-}
+recuperaElemento :: Eq a => [a] -> Int -> a
+recuperaElemento (x:_) 0 = x
+recuperaElemento [] _ = error "No existe el elemento"
+recuperaElemento (x:xs) i =
+  if i >= 0 
+  then recuperaElemento xs (i - 1)
+  else error "No existe el índice"
+
+
+{- Función: rota
+   Desripción: Regresa la lista dada trasladando n veces el
+   primer elemento de la lista al final 
+   Uso: rota [1,2,3,4,5] 2 = [3,4,5,1,2]
+        rota [1,2,3,4,5] (-1) = [5,1,2,3,4]
+-}
+rota :: [a] -> Int -> [a]
+rota [] _ = []
+rota xs n 
+  | n > 0     = rota (tail xs ++ [head xs]) (n - 1)
+  | n < 0     = rota (last xs:init xs) (n + 1)
+  | otherwise = xs
+
+
+{- Función: extranio
+   Desripción: 
+   Uso: extranio 13 = [13,40,20,10,5,16,8,4,2,1]
+-}
+extranio :: Int -> [Int]
+extranio 1 = [1]
+extranio n =
+  if (esPar n)
+  then n:(extranio (dividirPar n 2))
+  else n:(extranio (suma 1 (multi n 3)))
